5325e64896756d03b0145addb00fab1c4a0376b3
Don Reeves <donjreeves@gmail.com>
2022-01-15 13:35:34 -0500

    html/JS code added

--- script.js
+++ script.js
@@ -1,1 +1,66 @@
 // Mechanics for SmokeAlert Game
+function move(element) {
+    element.style.position = 'fixed'
+
+    function moveToCoordinates(left, bottom) {
+        element.style.left = left + 'px'
+        element.style.bottom = bottom + 'px'
+    }
+
+    function moveWithArrowKeys(left, bottom, callback){
+        let direction = null;
+        let x = left;
+        let y = bottom;
+    
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+        
+        function moveCharacter(){ 
+            if(direction === 'west' && character.x >= window.screenLeft){
+                x-=1
+            }
+            if(direction === 'north' && character.y >= window.screenTop){
+                y+=1
+            }
+            if(direction === 'east' && character.x <= 1000){
+                x+=1
+            }
+            if(direction === 'south' && character.y <= 800){
+                y-=1
+            }
+            element.style.left = x + 'px'
+            element.style.bottom = y + 'px'
+        }
+        
+        setInterval(moveCharacter, 1)
+        
+        document.addEventListener('keydown', function(e){
+            if(e.repeat) return;
+        
+            if(e.key === 'ArrowLeft'){
+                direction = 'west'
+            }
+            if(e.key === 'ArrowUp'){
+                direction = 'north'
+            }
+            if(e.key === 'ArrowRight'){
+                direction = 'east'
+            }
+            if(e.key === 'ArrowDown'){
+                direction = 'south'
+            }
+            callback(direction)
+            
+        })
+        
+        document.addEventListener('keyup', function(e){
+            direction = null
+            callback(direction)
+        })
+    }
+
+    return {
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
+    }
+}
