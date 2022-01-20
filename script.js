ac2f4f469682c608472b93d76e12a3cac0159034
Don Reeves <donjreeves@gmail.com>
2022-01-19 19:36:33 -0500

    fire gif and images added

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 const pc = newPlayableCharacter(1500, 100)
+
+move(newImage('assets/cupboard.png')).to(500, 175)
+move(newImage('assets/cupboard.png')).to(100, 275)
+move(newImage('assets/stove.png')).to(1200, 400)
+
+move(newImage('assets/Extras/fire.gif')).to(1200, 450)
+move(newImage('assets/Extras/fire.gif')).to(1210, 450)
+move(newImage('assets/Extras/fire.gif')).to(1220, 450)
+move(newImage('assets/Extras/fire.gif')).to(1230, 450)
+move(newImage('assets/Extras/fire.gif')).to(1240, 450)
