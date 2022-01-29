f2341425e81bf417a95c6d38c468d40b1e981494
Don Reeves <donjreeves@gmail.com>
2022-01-29 15:00:32 -0500

    updated files

--- fire.js
+++ fire.js
@@ -2,6 +2,8 @@
 let shootingWater = false
 
 function fire(url){
+    let item = newImage(url)
+   
     item.addEventListener('click', (e) =>{
         if(!shootingWater) return
         let element = document.getElementsByClassName("char")
@@ -9,10 +11,19 @@ function fire(url){
         console.log(charPos)
         console.log(e.clientX, e.clientY)
 
-        // if(fire.position.y - e <= charPos.position.y 
-        // && fire.position.x - e <= charPos.position.x)
-        item.remove()
+        // item.remove()
         
+        // if(item.position.x - charPos.position.x <= 1000
+        //     && item.position.y - charPos.position.y <= 1000){
+        //         console.log(item.position.x - charPos.position.x)
+        //     }
+
+        if(e.clientX - charPos.x <= 100
+            && e.clientY - charPos.y <= 100){
+                console.log(e.clientX - charPos.x)
+                item.remove()
+            }    
     })
     return item
 }
+
